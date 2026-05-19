517292ba1857f0bd31aa67a931d427980d19840b
Quek Zhi Qiang <zhi_qiang_quek@aiap.sg>
2026-05-19 17:50:55 +0800

    Added ssl fix for import and updated model

--- config/config.py
+++ config/config.py
@@ -1,6 +1,9 @@
 """Configuration and LLM initialization."""
 
 import os
+from config.win_ssl_fix import apply_windows_ssl_fix
+
+apply_windows_ssl_fix()
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
@@ -9,7 +12,7 @@ load_dotenv()
 
 # OpenAI API - used for all agents
 llm = ChatOpenAI(
-    model="gpt-5.1",
+    model="gpt-5-mini",
     api_key=os.getenv("OPENAI_API_KEY"),
     temperature=0,
 )
